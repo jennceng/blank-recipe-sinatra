7f54347087bbf274960f42a71a0c482b295d8e28
Jenn Eng <Jenn@Jennifers-MacBook-Pro-2.local>
2016-05-31 15:44:01 -0400

    live code solution

--- server.rb
+++ server.rb
@@ -1,2 +1,21 @@
 require 'sinatra'
 require 'pry'
+
+get '/' do
+  redirect '/ingredients'
+end
+
+get '/ingredients' do
+  @ingredients = File.readlines('ingredients.txt')
+  erb :index
+end
+
+post '/ingredients' do
+  new_ingredient = params[:ingredient]
+  
+  File.open('ingredients.txt', 'a') do |file|
+    file.puts(new_ingredient)
+  end
+
+  redirect '/ingredients'
+end
